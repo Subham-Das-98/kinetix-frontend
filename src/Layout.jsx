722d85049fe27f003329d62cd19f29110267e3c1
Subham-Das-98 <das198subham@gamil.com>
2024-12-05 00:09:19 +0530

    add navbar.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,12 @@
-import React from "react"
+import React from "react";
+import Navbar from "./components/header/Navbar";
 
 function App() {
-
   return (
     <>
-      <div><h1 className="bg-slate-800">hi kinetix</h1></div>
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
